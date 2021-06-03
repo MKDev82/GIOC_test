3ca4f851b1b4058f1472514eaa9b054738aaeb7b
Maksym Kolesnyk <mkwebdev82@icloud.com>
2021-06-03 11:15:05 +0300

    Added compression module

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -53,16 +54,11 @@ app.use(xss());
 // Prevent parameter pollution
 app.use(
   hpp({
-    whitelist: [
-      'ratingAverage',
-      'ratingQuantity',
-      'duration',
-      'maxGroupSize',
-      'difficulty',
-      'price',
-    ],
+    whitelist: [],
   })
 );
+
+app.use(compression());
 
 // Test middleware
 app.use((req, res, next) => {
