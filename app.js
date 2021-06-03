8b67ed7734567df04f6519da51ebf0c258e9a5b4
Maksym Kolesnyk <mkwebdev82@icloud.com>
2021-06-03 11:27:58 +0300

    Cancel some console.logs

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(compression());
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
+  // console.log(req.cookies);
   next();
 });
 
